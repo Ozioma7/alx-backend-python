e6447e9419ae515d8ff4cf287424164db439f109
Ozioma7 <okonisaozioma@gmail.com>
2024-03-21 12:35:19 +0100

    async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Write asynchronous coroutine"""
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Asynchronous coroutine max_delay"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+
+    return delay
